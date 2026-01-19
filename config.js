46eb13d9a4d42daaf03679ac68946617775a96eb
sou1lah <ahmedsouilah572@gmail.com>
2026-01-19 17:24:17 +0100

    udpate

--- config.js
+++
@@ -1,3 +0,0 @@
-// Blog configuration
-window.__BLOG_PASSWORD = "ishowspeed"; // Change this to your desired password
-window.__GITHUB_TOKEN = ""; // Add your GitHub personal access token here for private repos
